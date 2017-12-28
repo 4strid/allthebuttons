60299ebd51f4df5c0c98ed5db10e577e79bbe408
Peter Fesz-Nguyen <pfesznguyen@gmail.com>
2017-12-27 18:53:17 -0700

    ultra test

--- test/functions.js
+++ test/functions.js
@@ -1,8 +1,14 @@
+function randomCloseby (context, events, done) {
+	context.vars.chunk = (Math.floor(Math.random() * 20) - 10) + ":" + (Math.floor(Math.random() * 20) - 10)
+	return done()
+}
+
 function randomChunk (context, events, done) {
 	context.vars.chunk = (Math.floor(Math.random() * 200) - 100) + ":" + (Math.floor(Math.random() * 200) - 100)
 	return done()
 }
 
 module.exports = {
+	randomCloseby: randomCloseby,
 	randomChunk: randomChunk,
 }
